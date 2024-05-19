9f82b3601c9ea6ea0adc099e69d63999dd1a3eda
devaziz1 <devaziz1@gmail.com>
2024-05-19 18:19:21 +0500

    first commit

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const usernameValidation = z
+  .string()
+  .min(2, "username must be at least 2 characters")
+  .max(20, " username must be no more than 20 characters")
+  .regex(/^[a-zA-Z0-9_-]+$/, "username must not comtain special characters");
+
+export const signUpSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" })
+    .max(20, { message: "Password must be no more than 20 characters" }),
+});
